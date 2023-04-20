b22c301c8c0157dd65bfe4fb03bbd5222d786585
Sobhan Bera <sobhanbera258@gmail.com>
2023-04-21 04:55:12 +0530

    style(LandingScreen.kt): remove unused imports and commented out code feat(LandingScreen.kt): add gradient background to LandingScreen feat(LandingScreen.kt): add SobyteStatusBar to LandingScreen

--- app/src/main/java/com/sobhanbera/noisymelo/sobyte/screens/auth/LandingScreen.kt
+++ app/src/main/java/com/sobhanbera/noisymelo/sobyte/screens/auth/LandingScreen.kt
@@ -1,34 +1,45 @@
 package com.sobhanbera.noisymelo.sobyte.screens.auth
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.sobhanbera.noisymelo.sobyte.R
+import com.sobhanbera.noisymelo.sobyte.components.SobyteStatusBar
 import com.sobhanbera.noisymelo.sobyte.controllers.ScreenController
-import com.sobhanbera.noisymelo.sobyte.model.ScreenParams
+import com.sobhanbera.noisymelo.sobyte.funextension.gradientBackground
 import com.sobhanbera.noisymelo.sobyte.ui.theme.extras
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun LandingScreen(
 	navController: NavController,
 	screenController: ScreenController
 ) {
-	Surface(
-		color = MaterialTheme.colors.extras.transparent
-	) {
-		Image(
-			painter = painterResource(id = R.drawable.logo_with_name_bold),
-			contentDescription = "",
-			modifier = Modifier
-				.width(145.dp)
-				.clickable { screenController.navigateTo(ScreenParams.SIGN_IN_SCREEN) }
+	Scaffold {
+		// change the status bar color, and blending it with the below gradient-background
+		SobyteStatusBar(
+			color = MaterialTheme.colors.extras.primaryGradients[0]
 		)
+
+		Surface(
+			color = MaterialTheme.colors.extras.transparent,
+			modifier = Modifier
+				.fillMaxSize()
+				.gradientBackground(
+					MaterialTheme.colors.extras.primaryGradients,
+					angle = 315f
+				),
+			contentColor = MaterialTheme.colors.extras.text,
+		) {
+	//		Image(
+	//			painter = painterResource(id = R.drawable.logo_with_name_bold),
+	//			contentDescription = "",
+	//			modifier = Modifier
+	//				.width(145.dp)
+	//		)
+		}
 	}
 }
+
