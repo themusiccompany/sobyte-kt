757420d08e5178e17b539f8df11e758fc513cb8e
Sobhan Bera <sobhanbera258@gmail.com>
2023-04-24 17:35:22 +0530

    🎨 style(ScreenNames.kt): reformat ScreenParams enum class The ScreenParams enum class has been reformatted to improve readability and maintainability. The title, activeIcon, and inActiveIcon properties have been added to the enum class to provide more information about the screens. The enum class has also been sorted alphabetically to improve organization.

--- app/src/main/java/com/sobhanbera/noisymelo/sobyte/model/ScreenNames.kt
+++ app/src/main/java/com/sobhanbera/noisymelo/sobyte/model/ScreenNames.kt
@@ -1,15 +1,51 @@
 package com.sobhanbera.noisymelo.sobyte.model
 
-enum class ScreenParams(val screenRouteName: String) {
-	AUTH_NAVIGATION("AUTH_NAVIGATION"),
-	LANDING_SCREEN("LANDING_SCREEN"),
-	SIGN_IN_SCREEN("SIGN_IN_SCREEN"),
-	SIGN_UP_SCREEN("SIGN_UP_SCREEN"),
-	MAIN_APP_NAVIGATION("MAIN_APP_NAVIGATION"),
-	MUSIC_PLAYER_SCREEN("MUSIC_PLAYER_SCREEN"),
-	HOME_EXPLORE_SCREEN("HOME_EXPLORE_SCREEN"),
-	SEARCH_SCREEN("SEARCH_SCREEN"),
-	PROFILE_SCREEN("PROFILE_SCREEN"),
-	SETTING_SCREEN("SETTING_SCREEN"),
-	EDIT_PROFILE_SCREEN("EDIT_PROFILE_SCREEN"),
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MusicNote
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.ui.graphics.vector.ImageVector
+
+enum class ScreenParams(
+	val screenRouteName: String,
+	val title: String,
+	val activeIcon: ImageVector? = null,
+	val inActiveIcon: ImageVector? = null
+) {
+	// all authentication related screens and its root navigation
+	AUTH_NAVIGATION("AUTH_NAVIGATION", "",),
+	LANDING_SCREEN("LANDING_SCREEN", "Landing"),
+	SIGN_IN_SCREEN("SIGN_IN_SCREEN", "Login"),
+	SIGN_UP_SCREEN("SIGN_UP_SCREEN", "Register"),
+
+	// all main app related screens and its root navigation
+	MAIN_APP_NAVIGATION("MAIN_APP_NAVIGATION", ""),
+	MUSIC_PLAYER_SCREEN(
+		"MUSIC_PLAYER_SCREEN",
+		"Sobyte",
+		Icons.Filled.MusicNote,
+		Icons.Outlined.MusicNote,
+	),
+	HOME_EXPLORE_SCREEN(
+		"HOME_EXPLORE_SCREEN",
+		"Explore",
+		Icons.Filled.Home,
+		Icons.Outlined.Home,
+	),
+	SEARCH_SCREEN(
+		"SEARCH_SCREEN",
+		"Search",
+		Icons.Filled.Search,
+		Icons.Outlined.Search,
+	),
+	PROFILE_SCREEN(
+		"PROFILE_SCREEN",
+		"You",
+		Icons.Filled.Person,
+		Icons.Outlined.Person,
+	),
+	SETTING_SCREEN("SETTING_SCREEN", "Settings"),
+	EDIT_PROFILE_SCREEN("EDIT_PROFILE_SCREEN", "Edit Profile"),
 }
